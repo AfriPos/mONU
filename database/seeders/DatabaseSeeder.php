81b3a1be2df399e2aeba1959e01ea4ce8ce5da4e
Craig <craigjohnson2402@gmail.com>
2025-02-27 15:58:12 +0300

    added developer permissions to developer

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,21 +5,23 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
-     */
+      * Seed the application's database.
+      */
     public function run(): void
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        $developer = User::factory()->create([
             'name' => 'Developer',
             'email' => 'dev@afripos.co.ke',
             'password' => bcrypt('Developer@10000!'),
         ]);
+
+        // Assign developer role
+        $developer->assignRole('Developer');
 
         // add credit record
         \App\Models\Credit::create([
